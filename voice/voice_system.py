3783984716546d257810476cb7808d39d0c7efc3
Web by zaini <zainzainrauf27@gmail.com>
2026-08-27 10:41:31 +0500

    Implement VoiceSystem class with listening methods

--- voice/voice_system.py
+++ voice/voice_system.py
@@ -1,1 +1,14 @@
+class VoiceSystem:
+    def __init__(self):
+        self.listening = False
 
+    def start_listening(self):
+        self.listening = True
+        return "Listening..."
+
+    def stop_listening(self):
+        self.listening = False
+        return "Listening stopped."
+
+
+voice = VoiceSystem()
