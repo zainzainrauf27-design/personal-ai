61d24e89d8d1a062c1ea56178702d603e22b7f86
Web by zaini <zainzainrauf27@gmail.com>
2026-08-27 10:49:25 +0500

    Implement SkillManager class for skill management

--- skills/skill_manager.py
+++ skills/skill_manager.py
@@ -1,1 +1,15 @@
+class SkillManager:
+    def __init__(self):
+        self.skills = {}
 
+    def register(self, name: str, skill):
+        self.skills[name] = skill
+
+    def get(self, name: str):
+        return self.skills.get(name)
+
+    def list_skills(self):
+        return list(self.skills.keys())
+
+
+skills = SkillManager()
